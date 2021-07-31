e175f7df809df0056f079dfa4f5f9f66b78ebdf0
Vincent Grossier <vincent.grossier@gmail.com>
2021-07-31 18:02:14 +0200

    Initial commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Vincent Grossier on 31/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
